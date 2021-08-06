e2c5cee23cd685f6391e0f12d43b3e1ca7f6bbdf
ismailPervez <freelanceismail001@gmail.com>
2021-08-06 10:47:51 -0700

    issue fixed

--- script.js
+++ script.js
@@ -44,127 +44,114 @@ sendMoneyBtn.addEventListener('click', (event) => {
     var password = document.querySelector('#send-money-form #password').value;
 
     if (username && amount && password) {
-        // var receiverBalance;
-        // var senderBalance;
-        // var receiverUsername;
-        // var senderUsername;
-        // var receiverCurrency;
-        // var senderCurrency;
         console.log(amount);
 
-        if (amount > 1) {
-            db.collection('users').where("email", "==", auth.currentUser.email).get()
-            .then(snapshot => {
-                snapshot.docs.map(doc => {
-                    // console.log("sender's details: ", doc.data())
-                    var senderCurrency = doc.data().currency;
-                    var senderBalance = doc.data().amount;
+        db.collection('users').where("email", "==", auth.currentUser.email).get()
+        .then(snapshot => {
+            snapshot.docs.map(doc => {
+                // console.log("sender's details: ", doc.data())
+                var senderCurrency = doc.data().currency;
+                var senderBalance = doc.data().amount;
 
-                    if (localStorage.getItem('send-money-details')) {
-                        var sendMoneyDetails = JSON.parse(localStorage.getItem('send-money-details'));
-                        sendMoneyDetails.push(senderCurrency, senderBalance);
-                        localStorage.setItem('send-money-details', JSON.stringify(sendMoneyDetails));
-                        // console.log(sendMoneyDetails);
-                    }
+                if (localStorage.getItem('send-money-details')) {
+                    var sendMoneyDetails = JSON.parse(localStorage.getItem('send-money-details'));
+                    sendMoneyDetails.push(senderCurrency, senderBalance);
+                    localStorage.setItem('send-money-details', JSON.stringify(sendMoneyDetails));
+                    // console.log(sendMoneyDetails);
+                }
 
-                    else {
-                        localStorage.setItem('send-money-details', JSON.stringify([]));
-                        var sendMoneyDetails = JSON.parse(localStorage.getItem('send-money-details'));
-                        sendMoneyDetails.push(senderCurrency, senderBalance);
-                        localStorage.setItem('send-money-details', JSON.stringify(sendMoneyDetails));
-                        // console.log(sendMoneyDetails);
-                    }
-                })
+                else {
+                    localStorage.setItem('send-money-details', JSON.stringify([]));
+                    var sendMoneyDetails = JSON.parse(localStorage.getItem('send-money-details'));
+                    sendMoneyDetails.push(senderCurrency, senderBalance);
+                    localStorage.setItem('send-money-details', JSON.stringify(sendMoneyDetails));
+                    // console.log(sendMoneyDetails);
+                }
             })
-            .catch(error => {
-                console.log(error)
-            })
+        })
+        .catch(error => {
+            console.log(error)
+        })
 
-            db.collection('users').where("username", "==", username).get().then(snapshot => {
-                snapshot.docs.map(doc => {
-                    // console.log(doc.data());
-                    var receiverCurrency = doc.data().currency;
-                    var receiverBalance = doc.data().amount;
-                    var receiverID = doc.id;
-                    
-                    if (localStorage.getItem('send-money-details')) {
-                        var sendMoneyDetails = JSON.parse(localStorage.getItem('send-money-details'));
-                        sendMoneyDetails.push(receiverCurrency, receiverBalance, receiverID);
-                        localStorage.setItem('send-money-details', JSON.stringify(sendMoneyDetails));
-                        // console.log(sendMoneyDetails);
-                    }
+        db.collection('users').where("username", "==", username).get().then(snapshot => {
+            snapshot.docs.map(doc => {
+                // console.log(doc.data());
+                var receiverCurrency = doc.data().currency;
+                var receiverBalance = doc.data().amount;
+                var receiverID = doc.id;
+                
+                if (localStorage.getItem('send-money-details')) {
+                    var sendMoneyDetails = JSON.parse(localStorage.getItem('send-money-details'));
+                    sendMoneyDetails.push(receiverCurrency, receiverBalance, receiverID);
+                    localStorage.setItem('send-money-details', JSON.stringify(sendMoneyDetails));
+                    // console.log(sendMoneyDetails);
+                }
 
-                    else {
-                        localStorage.setItem('send-money-details', JSON.stringify([]));
-                        var sendMoneyDetails = JSON.parse(localStorage.getItem('send-money-details'));
-                        sendMoneyDetails.push(receiverCurrency, receiverBalance, receiverID);
-                        localStorage.setItem('send-money-details', JSON.stringify(sendMoneyDetails));
-                        // console.log(sendMoneyDetails);
-                    }
-                })
-
-            })
-            .catch(error => {
-                console.log(error);
+                else {
+                    localStorage.setItem('send-money-details', JSON.stringify([]));
+                    var sendMoneyDetails = JSON.parse(localStorage.getItem('send-money-details'));
+                    sendMoneyDetails.push(receiverCurrency, receiverBalance, receiverID);
+                    localStorage.setItem('send-money-details', JSON.stringify(sendMoneyDetails));
+                    // console.log(sendMoneyDetails);
+                }
             })
 
-            // calculations, then update
-            // get the sendMoneyDetails
-            const sendMoneyDetails = JSON.parse(localStorage.getItem('send-money-details'));
-            var [senderCurrency, senderBalance, receiverCurrency, receiverBalance, receiverID] = sendMoneyDetails;
+        })
+        .catch(error => {
+            console.log(error);
+        })
 
-            // console.log(senderCurrency, senderBalance, receiverCurrency, receiverBalance);
-            if (senderCurrency !== receiverCurrency) {
-                // console.log('coming from two different states!');
-                // convert into receiver's currency
-                async function convertCurrency(sender_currency, receiver_currency, amount) {
-                    var res = await fetch(`http://data.fixer.io/api/latest?access_key=8269b4aad97af40d2bc213b009da5670`);
-                    var data = await res.json();
+        // calculations, then update
+        // get the sendMoneyDetails
+        const sendMoneyDetails = JSON.parse(localStorage.getItem('send-money-details'));
+        var [senderCurrency, senderBalance, receiverCurrency, receiverBalance, receiverID] = sendMoneyDetails;
 
-                    var senderToEuro = Math.round((amount / data.rates[senderCurrency]) * 100) / 100;
-                    console.log(senderToEuro)
-                    var euroToReceiver = Math.round((data.rates[receiverCurrency] * senderToEuro) * 100) / 100;
+        // console.log(senderCurrency, senderBalance, receiverCurrency, receiverBalance);
+        if (senderCurrency !== receiverCurrency) {
+            // console.log('coming from two different states!');
+            // convert into receiver's currency
+            async function convertCurrency(sender_currency, receiver_currency, amount) {
+                var res = await fetch(`https://data.fixer.io/api/latest?access_key=8269b4aad97af40d2bc213b009da5670`);
+                var data = await res.json();
 
-                    senderBalance -= amount;
-                    receiverBalance += euroToReceiver;
+                var senderToEuro = Math.round((amount / data.rates[senderCurrency]) * 100) / 100;
+                console.log(senderToEuro)
+                var euroToReceiver = Math.round((data.rates[receiverCurrency] * senderToEuro) * 100) / 100;
 
-                    // update sender
-                    db.collection('users').doc(auth.currentUser.uid).update({
-                        amount: senderBalance
-                    })
-
-                    db.collection('users').doc(receiverID).update({
-                        amount: receiverBalance
-                    })
-
-                    console.log('converted and db updated...');
-                } 
-
-                convertCurrency(senderCurrency, receiverCurrency, amount)
-            }
-
-            else {
-                // console.log('coming from the same state');
                 senderBalance -= amount;
-                receiverBalance += amount;
+                receiverBalance += euroToReceiver;
 
-                // update balance in db
                 // update sender
                 db.collection('users').doc(auth.currentUser.uid).update({
                     amount: senderBalance
                 })
 
-                // update receiver
                 db.collection('users').doc(receiverID).update({
                     amount: receiverBalance
-                }) 
-            }
+                })
+
+                console.log('converted and db updated...');
+            } 
+
+            convertCurrency(senderCurrency, receiverCurrency, amount)
         }
 
         else {
-            alert('please enter a valid amount!');
-        }
+            // console.log('coming from the same state');
+            senderBalance -= amount;
+            receiverBalance += amount;
 
+            // update balance in db
+            // update sender
+            db.collection('users').doc(auth.currentUser.uid).update({
+                amount: senderBalance
+            })
+
+            // update receiver
+            db.collection('users').doc(receiverID).update({
+                amount: receiverBalance
+            }) 
+        }
         // in the end
         localStorage.clear();
     }
