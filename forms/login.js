0a0790d819a9423464b22d1acbc7d3a88539cc1c
ismailPervez <freelanceismail001@gmail.com>
2021-08-06 09:35:00 -0700

    refactor

--- forms/login.js
+++ forms/login.js
@@ -11,23 +11,9 @@ loginBtn.addEventListener('click', (event) => {
 
     auth.signInWithEmailAndPassword(email, password)
         .then(cred => {
-            // console.log(cred.user.uid);
-            // once the user creation has happened successfully, we can add the current user into firestore with appropriate details
-            // db.collection('users').doc(auth.currentUser.uid)
-            //     .where("email", "==", "newguy@gmail.com")
-            //     .get()
-            //     .then(user => {
-            //         console.log(user);
-            //     })
-            //     .catch(error => {
-            //         console.log(error);
-            //     })
-            // localStorage.setItem('message', `you have been successfully logged in with ${email}`);
-            // redirect to home page
-            // var alert = document.querySelector('.alert-success');
             loginForm.reset();
-            // window.location.replace('/accounts/index.html')
-            // alert.textContent = `you have been successfully logged in with ${email}`;
+            window.location.replace('../index.html')
+            alert.textContent = `you have been successfully logged in with ${email}`;
         })
         .catch(error => {
             console.log(error)
